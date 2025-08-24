31e748c1eb747c1f1cef573fdb73def41e4dcbfc
Seyed Amir Mousavi <sayed1384mahdi@gmail.com>
2025-08-25 00:39:04 +0330

    Add post_save signal to automatically create a Profile for new users

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import (
     BaseUserManager
 )
 from django.utils.translation import gettext_lazy as _
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class UserManager(BaseUserManager):
     """
@@ -76,3 +77,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.email
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    """
+    A signal receiver triggered after a User model is saved and
+    Automatically creates a Profile for newly registered users.
+    """
+    if created:
+        Profile.objects.create(user=instance, pk=instance.pk)
